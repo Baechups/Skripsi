5cdc405b37abaf2b87674a527ba5433ffe09897f
Baechups <indrahero.untoro27@gmail.com>
2022-12-09 17:44:06 +0700

    commit 9 12 22

---
+++ Source/Skripsi/SkripsiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkripsiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKRIPSI_API ASkripsiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
